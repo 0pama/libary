07123e7dfae8361641acb7bc857a2a95bb992c45
0pama <95030993+0pama@users.noreply.github.com>
2023-12-26 07:10:58 -0800

    copy v1

---
+++ index.js
@@ -0,0 +1,12 @@
+const showBtn = document.getElementById("addbook");
+const dialog = document.getElementById("favDialog");
+const jsCloseBtn = dialog.querySelector("#js-close");
+
+showBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+jsCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
